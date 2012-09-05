e98408150cbd7ecbd6930047350a3906aea1de23
Jake Schoermer <jake@secretservers.net>
2012-09-05 15:40:39 +1000

    Started Writing Scripts

---
+++ README.markdown
@@ -0,0 +1,3 @@
+Requires the Following Gems:
+
+CSV
---
+++ scripts/athletes.rb
@@ -0,0 +1,15 @@
+
+class Athletes
+
+	@athletefile = CSV.open('../data_source/Athlete.csv')
+
+	def initialize
+		puts "Athelete Script"
+	end
+
+
+
+end
+
+
+Athletes.new
