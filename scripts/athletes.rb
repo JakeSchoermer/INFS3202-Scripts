81772eb1fda8cbbba92f0f8825545c96aa799735
Jake Schoermer <jake@secretservers.net>
2012-10-01 13:43:08 +1000

    Updates

--- scripts/athletes.rb
+++ scripts/athletes.rb
@@ -1,14 +1,35 @@
+#!/usr/bin/env ruby
+
+$: << "." #Append Current Directory to Load Path 
+
+
+require "csv"
+#require "country_code"
 
 class Athletes
 
-	@athletefile = CSV.open('../data_source/Athlete.csv')
-
+  
+  @athletes = {}
+  
+  
 	def initialize
 		puts "Athelete Script"
-	end
+		
+		filepath = "../data_source/Athlete.csv"
+
+    
+    
+    @rows = []
+    data = CSV.open(filepath, :headers => true) do |line|
+      line.each do |row|
+        #puts header
+      end
+    end
+    
+    row.headers
 
 
-
+  end
 end
 
 
