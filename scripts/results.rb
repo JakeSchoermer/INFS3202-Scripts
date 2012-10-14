97032176338e77aec31cdf2b8ade1aac2b2991b9
Jake Schoermer <jake@secretservers.net>
2012-10-14 19:44:19 +1000

    Added Results Script -- needs to map foreign keys though

---
+++ scripts/results.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+
+$: << "." #Append Current Directory to Load Path
+
+
+require "csv"
+require "countries"
+
+class Results
+  include Countries
+
+  @results = []
+
+  def initialize
+
+    filepath = "../data_source/Results.csv"
+
+    rows = []
+
+    CSV.foreach(filepath, :headers => true, :return_headers => true,
+                :converters => :all)do |row|
+      #puts row
+      rows << row
+    end
+      @results = rows
+
+  end
+
+
+  def getEventId id
+    @results[id]["EventID"]
+  end
+
+  def getOlympicYear id
+    @results[id]["OlympicYear"]
+  end
+
+  def getOlympicSeason id
+    @results[id]["OlympicSeason"]
+  end
+
+  def getResult id
+    result = @results[id]["Result"]
+
+    if result.to_s.downcase == "n/a"
+      ""
+    else
+      result
+    end
+
+  end
+
+  def getMedal id
+    @results[id]["Medal"]
+  end
+
+  def writeOutput
+    filepath = "../output/Results.csv"
+
+    CSV.open(filepath, "wb") do |csv|
+
+      @results.each_index {|index|
+        if index >0
+          csv << ["#{index}", getEventId(index), getOlympicYear(index),
+                  getOlympicSeason(index), getResult(index), getMedal(index)]
+        end
+      }
+
+    end
+  end
+end
